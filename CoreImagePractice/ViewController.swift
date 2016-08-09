4ecd482f93b9ec229ae058219f7650ee1f07706f
张科 <zhangke19880503@163.com>
2016-08-09 21:45:59 +0800

    CoreImagePractice

---
+++ CoreImagePractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreImagePractice
+//
+//  Created by zhangke on 16/8/9.
+//  Copyright © 2016年 zhangke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
